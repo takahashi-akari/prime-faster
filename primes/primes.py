097054fc3899ccb8f095972768a62b50b2aacd91
Takahashi Akari <akaritakahashioss@gmail.com>
2022-08-08 05:22:54 +0900

    v0.0.1

--- primes/primes.py
+++ primes/primes.py
@@ -10,17 +10,31 @@ import math
 import numpy
 
 def is_prime(n):
+    if n < 2:
+        return False
     if n == 2:
         return True
-    if n % 2 == 0 or n < 2:
+    if n % 2 == 0:
         return False
-    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 def get_primes(n):
+    n -= 1
+    if n < 2:
+        return []
+    if n == 2:
+        return [2]
+    if n == 3:
+        return [2, 3]
     sieve = numpy.ones(n // 3 + (n % 6 == 2), dtype=bool)
     for i in range(1, int(n ** 0.5) // 3 + 1):
         if sieve[i]:
             k = 3 * i + 1 | 1
             sieve[k * k // 3 :: 2 * k] = False
             sieve[k * ( k - 2 * (i & 1 ) + 4) // 3 :: 2 * k] = False
-    return numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1)|1)]
+    np = numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1)|1)]
+    # return array
+    return np.tolist()
